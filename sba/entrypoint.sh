69917ea6bd33fabe18ce84dfaf12a3aa75e35826
Melody Bugnon <melodybugnon@gmail.com>
2024-03-12 11:20:15 +0100

    requete api local ok

--- sba/entrypoint.sh
+++ sba/entrypoint.sh
@@ -2,7 +2,10 @@
 
 echo "Effectuer les migrations de la base de données..."
 python manage.py makemigrations && python manage.py migrate
+python manage.py collectstatic --noinput
+
 
 echo "Lancer le serveur"
 
 gunicorn sba.wsgi:application --workers=2 --timeout 120 --bind=0.0.0.0:8001
+# python manage.py runserver 0.0.0.0:8000
